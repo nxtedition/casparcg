43aa54be69b69798144b57e0a3aaa8dbdb0ee409
Niklas P Andersson <3985238+niklaspandersson@users.noreply.github.com>
2024-12-06 21:17:12 +0100

    fix audio issue related to 16 vs 8 channels

--- src/modules/ffmpeg/util/audio_resampler.cpp
+++ src/modules/ffmpeg/util/audio_resampler.cpp
@@ -11,10 +11,11 @@ namespace caspar::ffmpeg {
 AudioResampler::AudioResampler(int sample_rate, AVSampleFormat in_sample_fmt)
 {
     AVChannelLayout channel_layout = AV_CHANNEL_LAYOUT_7POINT1;
-    
+    AVChannelLayout channel_layout_out = AV_CHANNEL_LAYOUT_HEXADECAGONAL;
+
     SwrContext* raw_ctx = nullptr;
     FF(swr_alloc_set_opts2(&raw_ctx,
-                           &channel_layout,
+                           &channel_layout_out,
                            AV_SAMPLE_FMT_S32,
                            sample_rate,
                            &channel_layout,
@@ -30,7 +31,7 @@ AudioResampler::AudioResampler(int sample_rate, AVSampleFormat in_sample_fmt)
 
 caspar::array<int32_t> AudioResampler::convert(int frames, const void** src)
 {
-    auto result = caspar::array<int32_t>(frames * 8 * sizeof(int32_t));
+    auto result = caspar::array<int32_t>(frames * 16 * sizeof(int32_t));
     auto ptr    = result.data();
     swr_convert(ctx.get(), (uint8_t**)&ptr, frames, reinterpret_cast<const uint8_t**>(src), frames);
 
